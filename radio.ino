0dc0a45f3610c1668115d484bdf361d3595b40e1
robobluebird <schroza@gmail.com>
2023-07-20 19:20:29 -0400

    now with 11 notes (need to find room for 12th) and basic functionality restored

--- radio.ino
+++ radio.ino
@@ -65,9 +65,9 @@ byte s11, s11_latch, bd11;
 byte ls11 = HIGH;
 unsigned long ld11 = 0;
 
-byte s12, s12_latch, bd12;
-byte ls12 = HIGH;
-unsigned long ld12 = 0;
+// byte s12, s12_latch, bd12;
+// byte ls12 = HIGH;
+// unsigned long ld12 = 0;
 
 byte sf;                // state of function button
 byte lsf = HIGH;        // last state of function button
@@ -118,37 +118,6 @@ byte mp = 3;                         // max number of samples that can be playin
 bool (*note1)() = NULL;
 bool (*note2)() = NULL;
 bool (*note3)() = NULL;
-
-void reset_indexes_and_accumulators(byte reverse) {
-  if (reverse) {
-    index1 = window_end;
-    index2 = window_end;
-    index3 = window_end;
-    index4 = window_end;
-    index5 = window_end;
-    index6 = window_end;
-    index7 = window_end;
-    index8 = window_end;
-  } else {
-    index1 = window_start;
-    index2 = window_start;
-    index3 = window_start;
-    index4 = window_start;
-    index5 = window_start;
-    index6 = window_start;
-    index7 = window_start;
-    index8 = window_start;
-  }
-
-  accumulator1 = 0;
-  accumulator2 = 0;
-  accumulator3 = 0;
-  accumulator4 = 0;
-  accumulator5 = 0;
-  accumulator6 = 0;
-  accumulator7 = 0;
-  accumulator8 = 0;
-}
 
 void set_ram_byte_mode() {
   digitalWrite(9, LOW);
@@ -221,7 +190,7 @@ bool update1() {
   if (index1 > window_end) {
     index1 = window_start;
     accumulator1 = 0;
-    return true;
+    return !(continuous && !s1);
   }
 
   return false;
@@ -234,12 +203,12 @@ bool update2() {
 
   index2 = window_start + (accumulator2 >> (6));
 
-  if (index2 > window_end) {
+  if (index2 > window_end || (continuous && s2)) {
     index2 = window_start;
     accumulator2 = 0;
     s2_latch = 0;
 
-    return true;
+    return !(continuous && !s2);
   }
 
   return false;
@@ -252,11 +221,11 @@ bool update3() {
 
   index3 = window_start + (accumulator3 >> (6));
 
-  if (index3 > window_end) {
+  if (index3 > window_end || (continuous && s3)) {
     index3 = window_start;
     accumulator3 = 0;
     s3_latch = 0;
-    return true;
+    return !(continuous && !s3);
   }
 
   return false;
@@ -269,11 +238,11 @@ bool update4() {
 
   index4 = window_start + (accumulator4 >> (6));
 
-  if (index4 > window_end) {
+  if (index4 > window_end || (continuous && s4)) {
     index4 = window_start;
     accumulator4 = 0;
     s4_latch = 0;
-    return true;
+    return !(continuous && !s4);
   }
 
   return false;
@@ -286,11 +255,11 @@ bool update5() {
 
   index5 = window_start + (accumulator5 >> (6));
 
-  if (index5 > window_end) {
+  if (index5 > window_end || (continuous && s5)) {
     index5 = window_start;
     accumulator5 = 0;
     s5_latch = 0;
-    return true;
+    return !(continuous && !s5);
   }
 
   return false;
@@ -303,11 +272,11 @@ bool update6() {
 
   index6 = window_start + (accumulator6 >> (6));
 
-  if (index6 > window_end) {
+  if (index6 > window_end || (continuous && s6)) {
     index6 = window_start;
     accumulator6 = 0;
     s6_latch = 0;
-    return true;
+    return !(continuous && !s6);
   }
 
   return false;
@@ -320,11 +289,11 @@ bool update7() {
 
   index7 = window_start + (accumulator7 >> (6));
 
-  if (index7 > window_end) {
+  if (index7 > window_end || (continuous && s7)) {
     index7 = window_start;
     accumulator7 = 0;
     s7_latch = 0;
-    return true;
+    return !(continuous && !s7);
   }
 
   return false;
@@ -337,11 +306,11 @@ bool update8() {
 
   index8 = window_start + (accumulator8 >> (6));
 
-  if (index8 > window_end) {
+  if (index8 > window_end || (continuous && s8)) {
     index8 = window_start;
     accumulator8 = 0;
     s8_latch = 0;
-    return true;
+    return !(continuous && !s8);
   }
 
   return false;
@@ -354,20 +323,71 @@ bool update9() {
 
   index9 = window_start + (accumulator9 >> (6));
 
-  if (index9 > window_end) {
+  if (index9 > window_end || (continuous && s9)) {
     index9 = window_start;
     accumulator9 = 0;
     s9_latch = 0;
-    return true;
+    return !(continuous && !s9);
   }
 
   return false;
 }
 
+bool update10() {
+  sample_out_temp += read_ram_byte(index10) - 127;
+
+  accumulator10 += major_sixth;
+
+  index10 = window_start + (accumulator10 >> (6));
+
+  if (index10 > window_end || (continuous && s10)) {
+    index10 = window_start;
+    accumulator10 = 0;
+    s10_latch = 0;
+    return !(continuous && !s10);
+  }
+
+  return false;
+}
+
+bool update11() {
+  sample_out_temp += read_ram_byte(index11) - 127;
+
+  accumulator11 += major_seventh;
+
+  index11 = window_start + (accumulator11 >> (6));
+
+  if (index11 > window_end || (continuous && s11)) {
+    index11 = window_start;
+    accumulator11 = 0;
+    s11_latch = 0;
+    return !(continuous && !s11);
+  }
+
+  return false;
+}
+
+// bool update12() {
+//   sample_out_temp += read_ram_byte(index12) - 127;
+
+//   accumulator12 += major_seventh;
+
+//   index12 = window_start + (accumulator12 >> (6));
+
+//   if (index12 > window_end || (continuous && s12)) {
+//     index12 = window_start;
+//     accumulator12 = 0;
+//     s12_latch = 0;
+//     return !(continuous && !s12);
+//   }
+
+//   return false;
+// }
+
 void setup(void) {
   cli();  // disable Arduino interrupts to configure things without being interrupted
 
-  // Use Arduino internal pullups to reduce necessary components. That means a button press will make a 0 and a button not-pressed will be 1
+  // Use Arduino internal pullups to reduce necessary components. That means a button press will make a 0 and a button not-pressed will be 1!
   pinMode(0, INPUT_PULLUP);
   pinMode(1, INPUT_PULLUP);
   pinMode(2, INPUT_PULLUP);
@@ -378,8 +398,11 @@ void setup(void) {
   pinMode(7, INPUT_PULLUP);
   pinMode(8, INPUT_PULLUP);
   pinMode(A1, INPUT_PULLUP);
+  pinMode(A2, INPUT_PULLUP);
+  pinMode(A3, INPUT_PULLUP);
+  // pinMode(A4, INPUT_PULLUP);
 
-  pinMode(A5, OUTPUT);  // You can use analog pins as digital pins if you want
+  pinMode(A5, OUTPUT);
 
   // CS pin for eeprom
   pinMode(9, OUTPUT);
@@ -454,7 +477,7 @@ ISR(TIMER2_COMPA_vect) {
 
   sample_out = sample_out_temp;
 
-  delay_buffer[delay_buffer_index] = sample_out >> 1;
+  delay_buffer[delay_buffer_index] = (sample_out >> 1);
 
   digitalWrite(10, LOW);  // CS pin for DAC LOW to start transaction
 
@@ -543,6 +566,10 @@ void loop(void) {
   byte d7 = digitalRead(0);
   byte d8 = digitalRead(1);
   byte d9 = digitalRead(A1);
+  byte d10 = digitalRead(A2);
+  byte d11 = digitalRead(A3);
+  // byte d12 = digitalRead(A4);
+
   byte df = digitalRead(8);
 
   unsigned long time = millis();
@@ -590,7 +617,6 @@ void loop(void) {
     if (!s3) {
       if (!sf) {
         continuous = !continuous;
-        reset_indexes_and_accumulators(0);
       } else {
         index3 = window_start;
         accumulator3 = 0;
@@ -608,7 +634,6 @@ void loop(void) {
       if (!sf) {
         reverse = !reverse;
         boomerang = 0;
-        reset_indexes_and_accumulators(1);
       } else {
         index4 = window_start;
         accumulator4 = 0;
@@ -626,7 +651,6 @@ void loop(void) {
       if (!sf) {
         boomerang = !boomerang;
         reverse = 0;
-        reset_indexes_and_accumulators(0);
       } else {
         index5 = window_start;
         accumulator5 = 0;
@@ -643,7 +667,6 @@ void loop(void) {
     if (!s6) {
       if (!sf) {
         am = !am;
-        reset_indexes_and_accumulators(0);
       } else {
         index6 = window_start;
         accumulator6 = 0;
@@ -688,6 +711,42 @@ void loop(void) {
     }
   }
 
+  if (d10 == ls10) {
+    ld10 = time;
+  } else if (time - ld10 > debounceDelay) {
+    s10 = d10;
+
+    if (!s10) {
+      index10 = window_start;
+      accumulator10 = 0;
+      assign_note(update10);
+    }
+  }
+
+  if (d11 == ls11) {
+    ld11 = time;
+  } else if (time - ld11 > debounceDelay) {
+    s11 = d11;
+
+    if (!s11) {
+      index11 = window_start;
+      accumulator11 = 0;
+      assign_note(update11);
+    }
+  }
+  
+  // if (d12 == ls12) {
+  //   ld12 = time;
+  // } else if (time - ld12 > debounceDelay) {
+  //   s12 = d12;
+
+  //   if (!s12) {
+  //     index12 = window_start;
+  //     accumulator12 = 0;
+  //     assign_note(update12);
+  //   }
+  // }
+
   if (df == lsf) {
     ldf = time;
   } else if (time - ldf > debounceDelay) {
@@ -703,6 +762,9 @@ void loop(void) {
   ls7 = s7;
   ls8 = s8;
   ls9 = s9;
+  ls10 = s10;
+  ls11 = s11;
+  // ls12 = s12;
 
   lsf = sf;
 
